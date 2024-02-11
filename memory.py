2a180e3f85e3b5a2af74e49c2bfb7113621856d1
Colonel_Jerry <leo.m922@stu.nebo.edu>
2024-02-10 19:18:51 -0700

    Basic read and write functions, more to add later

--- memory.py
+++ memory.py
@@ -10,11 +10,12 @@ def parse_file(self, filename): #parses the file and creates the dictionary aka 
     except FileNotFoundError:
         print("file not found")
 
-def read(self, command):
+def read(self, location):
     """Reads a word from keyboard input then stores that word into memory"""
     newWord = input("Please enter a word! 4 digit signed int")
-    self.memDict[key] = newWord
+    self.memDict[location] = newWord
 
-def write(self, command):
+def write(self, location):
     """Prints a word from the given memory location"""
-    print(self.memDict[key]) # Key should be the second half of a given word?
+    print(self.memDict[location])
+    # Key should be the second half of a given word? Are we passing that or the full word into a function?

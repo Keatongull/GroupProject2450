6e31c6ab16114bfe31ecc49637856519406e7894
Colonel_Jerry <leo.m922@stu.nebo.edu>
2024-02-10 19:17:16 -0700

    Basic read and write functions. Will add more later

--- memory.py
+++ memory.py
@@ -9,3 +9,12 @@ def parse_file(self, filename): #parses the file and creates the dictionary aka 
             pass
     except FileNotFoundError:
         print("file not found")
+
+def read(self, command):
+    """Reads a word from keyboard input then stores that word into memory"""
+    newWord = input("Please enter a word! 4 digit signed int")
+    self.memDict[key] = newWord
+
+def write(self, command):
+    """Prints a word from the given memory location"""
+    print(self.memDict[key]) # Key should be the second half of a given word?

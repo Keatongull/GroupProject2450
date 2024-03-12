5a18d05e4493f37b39c2ce4983bd119c4bc11237
Colonel_Jerry <leo.m922@stu.nebo.edu>
2024-03-11 20:18:09 -0600

    Basic skeleton of our view controller

---
+++ controller.py
@@ -0,0 +1,27 @@
+from GUI import DataGUI
+from memory import Memory
+
+class ViewController:
+    def __init__(self):
+        self.curMemory = Memory()
+        self.view = DataGUI()
+        self.fileAddress = None
+
+    def runButtonClicked():
+        pass
+
+    def importButtonClicked():
+        pass
+
+    def saveButtonClicked():
+        pass
+
+    def outputToConsole():
+        pass
+
+    # More needed???
+
+
+
+
+    

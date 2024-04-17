1a3a5e0f7f7ce065c1b5497ed0ca212ba469d557
Keatongull <129561986+Keatongull@users.noreply.github.com>
2024-04-17 13:41:41 -0600

    Fixed the file tree to have diff dicts for dif paths

--- src/gui/gui.py
+++ src/gui/gui.py
@@ -94,15 +94,14 @@ class DataGUI:
                 self.memory_tree.item(selected_item, values=(current_line, new_value), text=new_value)
                 #do not update file_dict if there is not an active file
                 if self.viewController.file_address != "":
-                    self.viewController.file_dict.update({self.viewController.extract_filename(self.viewController.file_address): self.get_mem_data()})
-                    print(self.viewController.file_dict)
+                    self.viewController.file_dict.update({self.viewController.file_address: self.get_mem_data()})
         else:
             messagebox.showwarning("Warning", "Please select a cell to edit.")
 
     def update_file_tree(self):
         self.file_tree.delete(*self.file_tree.get_children())
         for _key in self.viewController.file_dict.keys():
-            self.file_tree.insert('', 'end', values=_key)
+            self.file_tree.insert('', 'end', values=self.viewController.extract_filename(_key))
 
 
 
